379636473c4af33e58a22962d25a8befc26f9b7e
maguette-sow <maguettesow066@gmail.com>
2023-08-28 20:44:28 +0000

    mes fichiers

---
+++ connexiondb.php
@@ -0,0 +1,12 @@
+
+ <?php
+try{
+    $pdo=new PDO("mysql:host=localhost;dbname=gestionstage","root","MAGUETTESOW1121999");
+}
+catch(Exception $e) {
+    die('Erreur de connexion :'.$e->getMessage());
+}
+?>
+
+
+
---
+++ menu.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href="index.php"> gestion des stagiaires</a>
+</div>
+    <ul class="nav navbar-nav">
+        <li><a href="stagiaires.php">Les stagiaire</a></li>
+        <li><a href="filières.php">Les filières</a></li>
+        <li><a href="utilisateurs.php">Les utilisateurs</a></li> 
+    </ul>
+    </div>
+</nav>
